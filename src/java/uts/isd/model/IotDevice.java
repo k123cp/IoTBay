15cf6e6a2cb9a948f5192f7171e8806607adefa0
John Cu <44492864+k123cp@users.noreply.github.com>
2023-05-17 14:17:45 +1000

    Add files via upload
    A1 submission

---
+++ src/java/uts/isd/model/IotDevice.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package uts.isd.model;
+
+/**
+ *
+ * @author John
+ */
+public class IotDevice {
+    private String name;
+    private String description;
+    private String price;
+
+    public IotDevice(String name, String description, String price) {
+        this.name = name;
+        this.description = description;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+    
+    
+}
---
+++ src/java/uts/isd/model/Order.java
@@ -0,0 +1,57 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package uts.isd.model;
+
+/**
+ *
+ * @author John
+ */
+public class Order {
+    private String orderId;
+    private String customerId;
+    private String status;
+    private String date;
+
+    public Order(String orderId, String customerId, String status, String date) {
+        this.orderId = orderId;
+        this.customerId = customerId;
+        this.status = status;
+        this.date = date;
+    }
+
+    public String getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(String orderId) {
+        this.orderId = orderId;
+    }
+
+    public String getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(String customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+    
+    
+}
